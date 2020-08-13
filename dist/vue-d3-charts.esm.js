04cbae6761009c960c5cba12308a27fa403f0178
Marcel FOKOUN <marcel.fokoun@two-i.fr>
2020-08-13 08:06:06 +0200

    Trying #5

--- dist/vue-d3-charts.esm.js
+++ dist/vue-d3-charts.esm.js
@@ -1029,20 +1029,15 @@ class d3linechart extends d3chart {
       // Point group
       let gp = this.g.selectAll('.chart__points-group--' + k).data(this.data).enter().append('g').attr('class', 'chart__points-group chart__points-group--linechart chart__points-group--' + k).attr('transform', d => `translate(${this.xScale(d.jsdate)},${this.cfg.height})`); // Hover point
 
-      gp.append('circle').attr('class', 'chart__point-hover chart__point-hover--linechart').attr('fill', 'transparent').attr('r', this.cfg.points.hoverSize) // .on('mouseover', (d, j) => {
-      //     this.tooltip.html(_ => {
-      //         if (this.tData[i].values && this.tData[i].values.length && this.tData[i].values[j].y !== undefined) {
-      //             const label = this.cfg.tooltip.labels && this.cfg.tooltip.labels[i]
-      //                 ? this.cfg.tooltip.labels[i]
-      //                 : k;
-      //             return `
-      //             <div>${label}: ${this.tData[i].values[j].y !== undefined ? this.tData[i].values[j].y : 0}</div>`
-      //         } else
-      //             return `<div></div>`
-      //     })
-      //         .classed('active', true);
-      // })
-      .on('mouseout', _ => {
+      gp.append('circle').attr('class', 'chart__point-hover chart__point-hover--linechart').attr('fill', 'transparent').attr('r', this.cfg.points.hoverSize).on('mouseover', (d, j) => {
+        this.tooltip.html(_ => {
+          if (this.tData && this.tData.length && this.tData[i] && this.tData[i].values && this.tData[i].values.length && this.tData[i].values[j].y !== undefined) {
+            const label = this.cfg.tooltip.labels && this.cfg.tooltip.labels[i] ? this.cfg.tooltip.labels[i] : k;
+            return `
+                            <div>${label}: ${this.tData[i].values[j].y !== undefined ? this.tData[i].values[j].y : 0}</div>`;
+          } else return `<div></div>`;
+        }).classed('active', true);
+      }).on('mouseout', _ => {
         this.tooltip.classed('active', false);
       }).on('mousemove', _ => {
         this.tooltip.style('left', window.event['pageX'] - 28 + 'px').style('top', window.event['pageY'] - 40 + 'px');
