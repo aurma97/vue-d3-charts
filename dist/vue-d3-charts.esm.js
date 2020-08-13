274c1ccecd618b38df98432194e5a1f488ee3782
Marcel FOKOUN <marcel.fokoun@two-i.fr>
2020-08-13 09:36:20 +0200

    Trying #16

--- dist/vue-d3-charts.esm.js
+++ dist/vue-d3-charts.esm.js
@@ -1034,7 +1034,7 @@ class d3linechart extends d3chart {
         gp.append('circle').attr('class', 'chart__point-hover chart__point-hover--linechart').attr('fill', 'transparent').attr('r', this.cfg.points.hoverSize).on('mouseover', (d, j) => {
           console.log("not null and not undefined");
           this.tooltip.html(_ => {
-            console.log(this.tData[i].values[j]);
+            console.log(this.tData[i].values);
 
             if (this.tData[i].values[j] && (this.tData[i].values[j].y !== undefined || this.tData[i].values[j].y !== null)) {
               console.log(this.tData[i].values[j].y);
--- src/linechart/d3.linechart.js
+++ src/linechart/d3.linechart.js
@@ -259,7 +259,7 @@ class d3linechart extends d3chart {
                     .on('mouseover', (d, j) => {
                         console.log("not null and not undefined")
                         this.tooltip.html(_ => {
-                            console.log(this.tData[i].values[j])
+                            console.log(this.tData[i].values)
                             if (this.tData[i].values[j] && (this.tData[i].values[j].y !== undefined || this.tData[i].values[j].y !== null)) {
                                 console.log(this.tData[i].values[j].y)
                                 const label = this.cfg.tooltip.labels && this.cfg.tooltip.labels[i]
