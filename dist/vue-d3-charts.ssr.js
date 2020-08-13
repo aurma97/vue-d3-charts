90ee0d947565ea0765fbfb27fbdb36fb9daa8fbf
Marcel FOKOUN <marcel.fokoun@two-i.fr>
2020-08-13 10:26:41 +0200

    Trying #24

--- dist/vue-d3-charts.ssr.js
+++ dist/vue-d3-charts.ssr.js
@@ -1323,7 +1323,7 @@ var d3linechart = /*#__PURE__*/function (_d3chart) {
 
         if (_this3.tData && _this3.tData.length && _this3.tData[i] && _this3.tData[i].values && _this3.tData[i].values.length && (_this3.tData && _this3.tData.length && _this3.tData[i] && _this3.tData[i].values && _this3.tData[i].values.length) !== undefined) {
           gp.append('circle').attr('class', 'chart__point-hover chart__point-hover--linechart').attr('fill', 'transparent').attr('r', _this3.cfg.points.hoverSize).on('mouseover', function (d, j) {
-            console.log(_this3.data, d);
+            console.log(_this3.data, _this3.cfg.values);
 
             _this3.tooltip.html(function (_) {
               if (_this3.tData[i].values && _this3.tData[i].values.length) {
